f5d2424a31f67de5a7cb2bc6be3572eb7121aa9f
daniel.jung <daniel.jung@ibm.com>
2022-06-17 16:18:39 +0200

    asdasdasd

--- src/stories/PlayButton.stories.jsx
+++ src/stories/PlayButton.stories.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { within, userEvent } from '@storybook/testing-library';
-
 import { PlayButton } from './PlayButton';
 
 export default {
   title: 'Example/PlayButton',
   component: PlayButton,
   parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
 };
 
 const Template = (args) => <PlayButton {...args} />;
 
-// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const PlayButtonDefault = Template.bind({});
 
-//sd
