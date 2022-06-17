d35cbceda568d0fedeae96ec0a40328ae3ef1254
daniel.jung <daniel.jung@ibm.com>
2022-06-17 16:10:21 +0200

    asd

--- src/stories/PlayButton.stories.jsx
+++ src/stories/PlayButton.stories.jsx
@@ -17,9 +17,4 @@ const Template = (args) => <PlayButton {...args} />;
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const PlayButtonDefault = Template.bind({});
 
-// export const LoggedIn = Template.bind({});
-// LoggedIn.play = async ({ canvasElement }) => {
-//   const canvas = within(canvasElement);
-//   const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-//   await userEvent.click(loginButton);
-// };
+//sd
